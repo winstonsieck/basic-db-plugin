c8d445340595892e0ce9685025491f3a4c5c9974
joriesieck <jsieck@globalcognition.org>
2019-05-29 11:04:11 -0400

    v2: reversed order of functions, renamed install_data to save_data, moved action hooks, changed action hook for save_data, randomized data order

--- pet-info.php
+++ pet-info.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Pet Info
-Version: 1.0
+Version: 2.0
 Description: Inserts a bunch of information about various pets into the database
 Author: Jorie Sieck
 Author URI: https://my.thinkeracademy.com
@@ -9,6 +9,45 @@ Author URI: https://my.thinkeracademy.com
 global $pi_db_version;
 $pi_db_version = '1.0';
 
+// Activation hook: calls function when a particular page loads
+add_action('genesis_before_content','pi_save_data');
+/*
+ * Puts the given data in the previously created table.
+ */
+function pi_save_data() {
+    $page_id = 30;
+    if(is_page($page_id)) {
+        global $wpdb;
+        $names = array('Unicorn', 'Pegasus', 'Pony','Asian dragon','Medieval dragon','Lion','Gryphon');
+        $types = array('Horse','Horse','Horse','Dragon','Dragon','Cat','Cat');
+        $descriptions = array('Spiral horn centered in forehead','Flying; wings sprouting from back',
+            'Very small; half the size of standard horse','Serpentine body','Lizard-like body','Large; maned',
+            'Lion body; eagle head; wings');
+        $prices = array(10000,15000,500,30000,30000,2000,25000);
+
+        $table_name = $wpdb->prefix . 'pet';
+        $used_js = array();
+        for($i=0;$i<sizeof($names);$i++) {
+            $j = rand(0,sizeof($names)-1);
+            while(in_array($j,$used_js)) {
+                $j = rand(0,sizeof($names));
+            }
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'name' => $names[$j],
+                    'type' => $types[$j],
+                    'description' => $descriptions[$j],
+                    'price' => $prices[$j],
+                )
+            );
+            array_push($used_js,$j);
+        }
+    }
+}
+
+// Activation hook: calls function on plugin activation
+register_activation_hook(__FILE__,'pi_install');
 /*
  * Creates an empty table in the database called wp_pet with the given columns.
  */
@@ -34,35 +73,3 @@ function pi_install() {
 
     add_option( 'pi_db_version', $pi_db_version );
 }
-
-/*
- * Puts the given data in the previously created table.
- */
-function pi_install_data() {
-    global $wpdb;
-    $names = array('Unicorn', 'Pegasus', 'Pony','Asian dragon','Medieval dragon','Lion','Gryphon');
-    $types = array('Horse','Horse','Horse','Dragon','Dragon','Cat','Cat');
-    $descriptions = array('Spiral horn centered in forehead','Flying; wings sprouting from back',
-        'Very small; half the size of standard horse','Serpentine body','Lizard-like body','Large; maned',
-        'Lion body; eagle head; wings');
-    $prices = array(10000,15000,500,30000,30000,2000,25000);
-
-    $table_name = $wpdb->prefix . 'pet';
-    for($i=0;$i<sizeof($names);$i++) {
-        $wpdb->insert(
-            $table_name,
-            array(
-                'name' => $names[$i],
-                'type' => $types[$i],
-                'description' => $descriptions[$i],
-                'price' => $prices[$i],
-            )
-        );
-    }
-}
-
-/*
- * Action hooks: calls both functions on plugin activation.
- */
-register_activation_hook(__FILE__,'pi_install');
-register_activation_hook(__FILE__,'pi_install_data');
